543f19d94176ca4e76c1dc8477d62d5f7a71d7f8
megawac <megawac@gmail.com>
2013-09-14 11:51:50 -0400

    Node demo is back to gamesurge

--- demo/node/config.js
+++ demo/node/config.js
@@ -1,6 +1,6 @@
 var options =  {
     DEBUG: true,
-    IRCSERVER: 'chat.freenode.net', //irc server adress
+    IRCSERVER: 'irc.gamesurge.net', //irc server adress
     IRCPORT: 6667, //irc servers port
     USE_WEBSOCKETS: true, //whether to use websockets - some servers dont support the protocol. Fallbacks are done through socket.io
     APP_PORT: process.env.PORT || 8080,
