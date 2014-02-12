cd5cb1882cbd301c4dcd78e35ed3334d2a65e238
megawac <megawac@gmail.com>
2014-02-12 17:50:47 -0500

    Simplify templates

--- templates/Templates.js
+++ templates/Templates.js
@@ -1,49 +1,48 @@
-
 (function(engine) {
 
-    //where to store these things
-    var source = {},
-        compiled = qwebirc.templates || {};
-
-    source.messageLine = "<hr class='lastpos' />";
 
 
-    //portions:
-    source.dropdownhint = "<div class='dropdownhint'>Click the icon for the main menu.</div>";
+    //Some simple templates that dont really need to be compiled
+    var source = {
+        messageLine:    "<hr class='lastpos' />",
+        dropdownhint:   "<div class='dropdownhint'>Click the icon for the main menu.</div>",
+        
+        tabbar:         "<div class='tabbar'></div>",
+        tabDetach:      "<span class='detach ui-icon ui-icon-newwin' title='" + lang.detachWindow + "'></span>",
+        tabAttach:      "<span class='attach ui-icon ui-icon-circle-minus'></span>",
+        tabClose:       "<span class='tab-close ui-icon ui-icon-circle-close' title='" + lang.closeTab + "'></span>",
 
-    source.tabbar = "<div class='tabbar'></div>";
-    source.tabDetach = "<span class='detach ui-icon ui-icon-newwin' title='" + lang.detachWindow + "'></span>";
-    source.tabAttach = "<span class='attach ui-icon ui-icon-circle-minus'></span>";
-    source.tabClose = "<span class='tab-close ui-icon ui-icon-circle-close' title='" + lang.closeTab + "'></span>";
+        loadingPage:    "<div class='loading'>" + lang.loadingPage + "<img src='images/loading.gif' alt='url'></div>"
+    };
 
-	source.loadingPage = "<div class='loading'>" + lang.loadingPage + "<img src='images/loading.gif' alt='url'></div>";
+    // source.verticalDivider = "<div class='ui-icon ui-icon-grip-solid-vertical handle vertical'></div>";
+    // source.horizontalDivider = "<div class='ui-icon ui-icon-grip-solid-horizontal handle horizontal'></div>";
 
 
-    source.verticalDivider = "<div class='ui-icon ui-icon-grip-solid-vertical handle vertical'></div>";
-    source.horizontalDivider = "<div class='ui-icon ui-icon-grip-solid-horizontal handle horizontal'></div>";
+    var compiled = qwebirc.templates || {};
 
     /************************
         HELPERS
     ***********************/
-    engine.registerHelper('check', function(checked, s2){
-        return checked ? 'checked' : '';
+    engine.registerHelper("check", function(checked){
+        return checked ? "checked" : "";
     });
 
-    engine.registerHelper('enableDisable', function(x) {
+    engine.registerHelper("enableDisable", function(x) {
         return x ? lang.DISABLE : lang.ENABLE;//if true shows disable
     });
 
-    engine.registerHelper('$link', util.formatURL);
+    engine.registerHelper("$link", util.formatURL);
 
     //f(property name, type of prop, default val)
-    engine.registerHelper('$css', function(prop, def, type, default2) {//this refers to context
-        if(type === "c") {//colour
+    engine.registerHelper("$css", function(prop, def, type, default2) {//this refers to context
+        if(type === "c") { //colour
             var x = new Color(def);
             var c = x.setHue(this.style_hue).setSaturation(x.hsb[1] + this.style_saturation).setBrightness(x.hsb[2] + this.style_brightness);
             if (Browser.ie && c == "255,255,255") c = "255,255,254";// IE confuses white with transparent... 
             
             return "rgb(" + c + ")";
-        } 
+        }
         else if(type === "comp") {
             return this[prop] ? def : default2;
         }
